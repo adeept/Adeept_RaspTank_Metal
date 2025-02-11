a2dd986d076286dcc02af9d31bf655b43bef136b
Adeept <support@adeept.com>
2025-02-11 14:11:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -146,22 +146,6 @@ for x in range(3):
     if mark_3 == 0:
         break
 
-# commands_3 = [
-#     "sudo pip3 install numpy --break-system-packages",
-#     #"sudo pip3 install opencv-contrib-python==3.4.3.18",
-#     # "sudo pip3 install opencv-contrib-python==3.4.17.61",
-#     "sudo pip3 install imutils zmq pybase64 psutil --break-system-packages"
-# ]
-
-# mark_3 = 0
-# for x in range(3):
-#     for command in commands_3:
-#         if os.system(command) != 0:
-#             print("Error running installation step 3")
-#             mark_3 = 1
-#     if mark_3 == 0:
-#         break
-
 
 try:
     replace_num("/boot/config.txt", '#dtparam=i2c_arm=on','dtparam=i2c_arm=on\nstart_x=1\n')
@@ -183,16 +167,6 @@ except:
 os.system("sudo chmod 777 /"+ user_home +"/startup.sh")
 replace_num('/etc/rc.local','fi','fi\n/'+ user_home +'startup.sh start')
 
-# try: #fix conflict with onboard Raspberry Pi audio
-#     os.system('sudo touch /etc/modprobe.d/snd-blacklist.conf')
-#     with open("/etc/modprobe.d/snd-blacklist.conf",'w') as file_to_write:
-#         file_to_write.write("blacklist snd_bcm2835")
-# except:
-#     pass
-# try:
-#     os.system("sudo cp -f /"+ user_home +"/adeept_rasptank/server/config.txt //etc/config.txt")
-# except:
-#     os.system("sudo cp -f "+ thisPath  +"/adeept_rasptank/server/config.txt //etc/config.txt")
 print('The program in Raspberry Pi has been installed, disconnected and restarted. \nYou can now power off the Raspberry Pi to install the camera and driver board (Robot HAT). \nAfter turning on again, the Raspberry Pi will automatically run the program to set the servos port signal to turn the servos to the middle position, which is convenient for mechanical assembly.')
 print('restarting...')
 os.system("sudo reboot")
